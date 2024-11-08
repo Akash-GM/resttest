1ac6760684ad79b4594dcce13671d6a1d5691adf
unknown <george.akash99@gmail.com>
2024-11-08 15:24:00 +0530

    added a new route for post modified get_user to fetch all users instead of dummy data

--- resttest/api/urls.py
+++ resttest/api/urls.py
@@ -1,8 +1,9 @@
 
-from .views import get_user
+from .views import get_user,create_user
 
 from django.urls import path
 
 urlpatterns= [
-    path('users/',get_user,name="get_user")
+    path('users/',get_user,name="get_user"),
+    path('users/create_user',create_user,name="create_user")
 ]
--- resttest/api/views.py
+++ resttest/api/views.py
@@ -6,7 +6,18 @@ from .serializer import UserSerializer
 
 @api_view(['GET'])
 def get_user(request):
-    return Response(UserSerializer({'name':'pedro',"age":22}).data)
+    users = User.objects.all()
+    serialized_users= UserSerializer(users,many= True)
+    return Response(serialized_users.data)
+
+@api_view(['POST'])
+def create_user(request):
+
+    serialized_data = UserSerializer(data=request.data)
+    if serialized_data.is_valid():
+        serialized_data.save()
+        return Response(serialized_data.data,status=status.HTTP_201_CREATED)
+    return Response(serialized_data.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 
